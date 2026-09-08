d10caabe478d83022a419fa43e19dc48f79fd893
Muqsith <muqsithpersonal@gmail.com>
2026-09-08 15:42:29 +0700

    fix(v4): Add susfs_def.h header and fix Reflex schedutil dependency

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2677,7 +2677,7 @@ enum schedutil_type {
 	ENERGY_UTIL,
 };
 
-#ifdef CONFIG_CPU_FREQ_GOV_SCHEDUTIL
+#if defined(CONFIG_CPU_FREQ_GOV_SCHEDUTIL) || defined(CONFIG_CPU_FREQ_GOV_REFLEX)
 
 unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 				 unsigned long max, enum schedutil_type type,
